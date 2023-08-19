1a23dae7da869c9fca324d5683131025467ba1ac
nat7216 <xukchen@ucm.es>
2023-08-19 15:57:39 +0200

    update SRCREV

--- recipes-examples/tensorflow-lite/files/tflite-examples-init.py
+++ recipes-examples/tensorflow-lite/files/tflite-examples-init.py
@@ -38,7 +38,9 @@ def main():
                     process = subprocess.Popen("python3 detect.py >>./log.log 2>&1", shell=True, cwd="/usr/share/tensorflow/lite/examples/python/ADAS-apps/code/", preexec_fn=os.setsid)
                     print("process created: " + str(process.pid))
                 elif name == "ButtonName.F3" and value == 0:
-                    pass
+                    kill_process(process)
+                    process = subprocess.Popen("python3 detect.py --do_recognize >>./log.log 2>&1", shell=True, cwd="/usr/share/tensorflow/lite/examples/python/ADAS-apps/code/", preexec_fn=os.setsid)
+                    print("process created: " + str(process.pid))
                 elif name == "ButtonName.O" and value == 0:
                     kill_process(process)
 
